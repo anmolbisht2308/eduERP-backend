916fef0bbeb00cd21cac9fcac4aade060b728396
anmolbisht2308 <anmolbisht7@gmail.com>
2026-01-30 03:00:19 +0530

    feat: init backend with auth module

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,12 @@ const app: Application = express();
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true,
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(helmet());
 app.use(cookieParser());
 
